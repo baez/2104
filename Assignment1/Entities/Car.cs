97eeffb6586838777b1ffa00e5d8583fcc66d9bc
Karina Zialcita <000428830@student.vcc.ca>
2022-01-13 15:39:59 -0800

    added car and vehicle classes

---
+++ Assignment1/Entities/Car.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1.Entities
+{
+    class Car: Vehicle
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+    }
+}
---
+++ Assignment1/Entities/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Assignment1
+{
+    public class Vehicle
+    {
+
+        public string Name { get; set; }
+        public string Capacity { get; set; }
+        public int Year { get; set; }
+        public int Mileage { get; set; }
+        public decimal Price { get; set; }
+    }
+}
